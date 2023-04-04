3f2a6035ed58dc8f6ca35422f1f5e160f6c50837
Mahamat04 <mahamatalhabib224@gmail.com>
2023-04-04 03:43:33 +0100

    Loading questions from open trivia db

--- game.js
+++ game.js
@@ -7,78 +7,60 @@ const scoreText = document.getElementById('score');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
+let progresCounter = 1;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "What is the name of the Linux Kernel creator?",
-        choice1: "Linux Torvalds",
-        choice2: "Bill Gate",
-        choice3: "Linus Torvalds",
-        choice4: "Richard Stallman",
-        answer: 3
-    },
-    {
-        question: "When did the Linux Kernel release?",
-        choice1: 1991,
-        choice2: 1990,
-        choice3: 2001,
-        choice4: 1989,
-        answer: 1
-    },
-    {
-        question: "What is Linux?",
-        choice1: "An operationg system",
-        choice2: "A task manager",
-        choice3: "A virtualization system",
-        choice4: "A kernel",
-        answer: 4
-    },
-    {
-        question: "What is a terminal?",
-        choice1: "A user interface with a shell",
-        choice2: "A video game",
-        choice3: "A text editor",
-        choice4: "A graphical user interface",
-        answer: 1
-    },
-    {
-        question: "Which of the following is not a Unix shell?",
-        choice1: "Bash",
-        choice2: "Psh",
-        choice3: "Ksh",
-        choice4: "Zsh",
-        answer: 2
-    },
-    {
-        question: "What is a Shell?",
-        choice1: "A command line interpreter",
-        choice2: "A program interpreter",
-        choice3: "A terminal",
-        choice4: "A command line",
-        answer: 1
+let questions = [];
+fetch("https://opentdb.com/api.php?amount=15&category=18&type=multiple").then(
+    res => {
+        console.log(res)
+        return res.json();
     }
-];
+).then(loadedQuestions => {
+        questions = loadedQuestions.results.map(loadedQuestion => {
+            const formatedQuestion = {
+                question: loadedQuestion.question
+        }
+
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+        formatedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+
+        answerChoices.splice(
+                            formatedQuestion.answer - 1, 
+                            0,
+                            loadedQuestion.correct_answer
+                    );   
+
+        answerChoices.forEach((choice, index) => {
+            formatedQuestion["choice" + (index + 1)] = choice;
+        }); 
+        return formatedQuestion;
+    });
+    startGame();
+}).catch( err => {
+    console.error(err);
+})
 
 //CONSTANTS
-
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 5;
 
 startGame = () => {
     questionCounter = 0;
+    score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
 }
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || availableQuestions.length <= MAX_QUESTIONS)
+    if(progresCounter > MAX_QUESTIONS)
     {
         localStorage.setItem('mostRecentScore', score);
         //go to the end page
         return window.location.assign('/end.html');
     }
-    questionCounter++;
+    questionCounter++;    
+    progresCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
     //updating the progress bar
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
@@ -94,6 +76,7 @@ getNewQuestion = () => {
 
     availableQuestions.splice(questionIndex, 1);
     acceptingAnswers = true;
+
 
 }
 
@@ -125,6 +108,4 @@ incrementScore = num => {
     score += num;
     scoreText.innerText = score;
 }
-
-startGame();
 
